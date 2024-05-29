613643a2820eb095ddda35e3f72d2523c1d509b3
michele laptop <micheledong2803@gmail.com>
2024-05-29 16:47:22 +0200

    m2 - tv series

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,19 @@ export default {
     }
   },
   methods: {
-    searchMovie() {
-
-      const options = {
+    search() {
+      const tv = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/search/tv',
+        params: {
+          query: this.searchText,
+          include_adult: 'true',
+          language: 'it-IT',
+          page: '1',
+          api_key: '1a03cede7fddf759ac17995c787cc6e0',
+        },
+      };
+      const movie = {
         method: 'GET',
         url: 'https://api.themoviedb.org/3/search/movie',
         params: {
@@ -24,9 +34,17 @@ export default {
       };
 
       axios
-        .request(options)
-        .then((response)=> {
+        .request(movie)
+        .then((response) => {
           this.store.searchFilm = response.data.results;
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+      axios
+        .request(tv)
+        .then((response) => {
+          this.store.searchTv = response.data.results;
         })
         .catch(function (error) {
           console.error(error);
@@ -51,24 +69,37 @@ export default {
 "vote_average": 5.7,
 "vote_count": 38 -->
 
+<!-- https://image.tmdb.org/t/p/original/Ag6qhzsJd3k1NKuNrG9RmhZDMh7.jpg -->
+
 <template>
 
   <div class="searchbar">
     <input type="text" v-model="searchText">
-    <button @click="searchMovie()">Submit</button>
+    <button @click="search()">Submit</button>
   </div>
 
-  <div class="movie" v-for="element in store.searchFilm" v-show="store.searchFilm!=[]">
+  <h2>MOVIES</h2>
+  <div class="movie" v-for="element in store.searchFilm" v-show="store.searchFilm != []">
     titolo: {{ element.title }} <br>
     titolo originale: {{ element.original_title }} <br>
     lingua originale: {{ element.original_language }} <br>
     voto: {{ element.vote_average }}
   </div>
 
+  <h2>TV SERIES</h2>
+  <div class="movie" v-for="element in store.searchTv" v-show="store.searchTv != []">
+    titolo: {{ element.name }} <br>
+    titolo originale: {{ element.original_name }} <br>
+    lingua originale: {{ element.original_language }} <br>
+    voto: {{ element.vote_average }}
+  </div>
+
+  <pre>{{ store.searchTv }}</pre>
+
 </template>
 
 <style scoped>
-.movie{
-  margin-bottom:3rem;
+.movie {
+  margin-bottom: 3rem;
 }
 </style>
