c163c477aca835cc37ae9a537da6d72828cbf930
michele laptop <micheledong2803@gmail.com>
2024-05-29 15:33:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import store from "./data/store.js"
+export default{
+  data(){
+    return{
+      data
+    }
+  },
+  methods:{
+    
+  },
+  created(){
+
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped>
+</style>
