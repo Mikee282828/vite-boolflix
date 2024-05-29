d3a6d26ef05d5df7118fb903b0d6d6078a07cd62
michele laptop <micheledong2803@gmail.com>
2024-05-29 15:49:17 +0200

    m1 - api implementation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <script>
-import store from "./data/store.js"
-export default{
-  data(){
-    return{
-      store
+import store from "./data/store.js";
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      store,
+      searchText: ""
     }
   },
-  methods:{
+  methods: {
+    searchMovie() {
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/search/movie',
+        params: { query: this.searchText, include_adult: 'true', language: 'it-IT', page: '1' },
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer 1a03cede7fddf759ac17995c787cc6e0'
+        }
+      };
 
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   },
-  created(){
+  created() {
 
   }
 }
@@ -17,11 +38,10 @@ export default{
 
 <template>
   <div class="searchbar">
-    <input type="text">
-    <button>Submit</button>
+    <input type="text" v-model="searchText">
+    <button @click="searchMovie()">Submit</button>
   </div>
   <pre></pre>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
