1965c615429e2cf852ed549d5038ab254b9d866a
michele laptop <micheledong2803@gmail.com>
2024-05-29 16:13:11 +0200

    m1 - movie info printed on page

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,23 @@ export default {
   },
   methods: {
     searchMovie() {
+
       const options = {
         method: 'GET',
         url: 'https://api.themoviedb.org/3/search/movie',
-        params: { query: this.searchText, include_adult: 'true', language: 'it-IT', page: '1' },
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer 1a03cede7fddf759ac17995c787cc6e0'
-        }
+        params: {
+          query: this.searchText,
+          include_adult: 'true',
+          language: 'it-IT',
+          page: '1',
+          api_key: '1a03cede7fddf759ac17995c787cc6e0',
+        },
       };
 
       axios
         .request(options)
-        .then(function (response) {
-          console.log(response.data);
+        .then((response)=> {
+          this.store = response.data.results;
         })
         .catch(function (error) {
           console.error(error);
@@ -36,12 +39,36 @@ export default {
 }
 </script>
 
+<!-- "id": 19603,
+"original_language": "en",
+"original_title": "Ciao",
+"overview": "A man learns that his late friend had a secret online lover who is on the way from Italy.",
+"popularity": 6.625,
+"poster_path": "/fvgR62s7bJizAS8xnGZCHmZrPbw.jpg",
+"release_date": "2008-12-05",
+"title": "Ciao",
+"video": false,
+"vote_average": 5.7,
+"vote_count": 38 -->
+
 <template>
+
   <div class="searchbar">
     <input type="text" v-model="searchText">
     <button @click="searchMovie()">Submit</button>
   </div>
-  <pre></pre>
+
+  <div class="movie" v-for="element in store">
+    titolo: {{ element.title }} <br>
+    titolo originale: {{ element.original_title }} <br>
+    lingua originale: {{ element.original_language }} <br>
+    voto: {{ element.vote_average }}
+  </div>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.movie{
+  margin-bottom:3rem;
+}
+</style>
